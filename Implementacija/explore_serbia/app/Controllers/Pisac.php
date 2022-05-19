bca1aa43e6ded760b024694db1a3056a39ce2867
NikolaBjelobaba <nikolabjelobaba980@gmail.com>
2022-05-19 14:27:44 +0200

    dodat kontroler pisca, i dodate funkcionalnosti prikaza sadrzaja, pretrage i izlogovanja piscu

--- Implementacija/explore_serbia/app/Controllers/Pisac.php
+++ Implementacija/explore_serbia/app/Controllers/Pisac.php
@@ -1,11 +1,149 @@
 <?php
 
+// by Nikola Bjelobaba 0442/2019
 namespace App\Controllers;
+
+use App\Models\ObjavaModel;
+use App\Models\KorisnikModel;
+use App\Models\ReklamaModel;
+use App\Models\LokacijaModel;
+use App\Models\ObjavaTagModel;
+use App\Models\TagModel;
+/**
+ * Pisac – klasa kontroler koja je odgovorna za funkcionalnosti pisca
+ *
+ * @version 1.0
+ */
 
 class Pisac extends BaseController
 {
-    public function index()
-    {
-        return view('welcome_message');
+    
+     /**
+     * Prikazuje zadati header i stranicu
+     * @param string $header Header 
+     * @param string $stranica Stranica
+     * @param array $podaci Podaci
+     *
+     * @return void
+     */
+    
+    protected function prikaz($header, $stranica, $podaci){
+        echo view("stranice/$header.php", $podaci);
+        echo view("stranice/$stranica.php", $podaci);
+    }
+    
+    
+    /**
+     * Prikazuje pocetnu stranicu sa svim objavama
+     *
+     * @return void
+     */
+    
+    public function index() {
+        $objavaModel = new ObjavaModel();
+        $korisnikModel = new KorisnikModel();
+        $objavaTagModel = new ObjavaTagModel();
+        $tagModel = new TagModel();
+        $objave = $objavaModel->orderBy('vremeKreiranja', 'desc')->where('odobrena', 1)->findAll();
+        
+        $autori = [];
+        $tagoviCssKlase = [];
+        foreach($objave as $objava){
+            $korisnickoIme = $objava->autor;
+            $autor = $korisnikModel->find($korisnickoIme);
+            array_push($autori, $autor);
+            
+            $obajaveTagovi = $objavaTagModel->where('objavaID', $objava->id)->findAll();
+            $tagCssKlasa = "";
+            foreach($obajaveTagovi as $objavaTag){
+                $tag = $tagModel->find($objavaTag->tagID);
+                switch((int)$tag->kategorija){
+                    case 1:
+                        $tagCssKlasa.=" istorijskaLicnost";
+                        break;
+                    case 2:
+                        $tagCssKlasa.=" spomenik";
+                        break;
+                    case 3:
+                        $tagCssKlasa.=" crkvaManastir";
+                        break;
+                    case 4:
+                        $tagCssKlasa.=" tvrdjava";
+                        break;
+                    case 5:
+                        $tagCssKlasa.=" areoloskoNalaziste";
+                        break;
+                    case 6:
+                        $tagCssKlasa.=" parkPrirode";
+                        break;
+                }
+            }
+            array_push($tagoviCssKlase, $tagCssKlasa);
+        }
+        
+        $this->prikaz("headerPisac", "objave", ["kontroler" => "Pisac", "objave" => $objave, "autori" => $autori, "tagoviCssKlase" => $tagoviCssKlase]);
+    }
+    
+    /**
+     * Prikazuje sve objave koje odgovaraju trazenom pojmu
+     *
+     * @return void
+     */
+    public function pretraga(){
+        $pretraga = $this->request->getVar("pretraga");
+        if ($pretraga == "") return redirect()->to("Pisac/");
+        
+        $objavaModel = new ObjavaModel();
+        $korisnikModel = new KorisnikModel();
+        $objavaTagModel = new ObjavaTagModel();
+        $tagModel = new TagModel();
+        $objave = $objavaModel->orderBy('vremeKreiranja', 'desc')->where('odobrena', 1)->like('naslov', $pretraga)->orLike('tekst', $pretraga)->findAll();
+        
+        $autori = [];
+        $tagoviCssKlase = [];
+        foreach($objave as $objava){
+            $korisnickoIme = $objava->autor;
+            $autor = $korisnikModel->find($korisnickoIme);
+            array_push($autori, $autor);
+            
+            $obajaveTagovi = $objavaTagModel->where('objavaID', $objava->id)->findAll();
+            $tagCssKlasa = "";
+            foreach($obajaveTagovi as $objavaTag){
+                $tag = $tagModel->find($objavaTag->tagID);
+                switch((int)$tag->kategorija){
+                    case 1:
+                        $tagCssKlasa.=" istorijskaLicnost";
+                        break;
+                    case 2:
+                        $tagCssKlasa.=" spomenik";
+                        break;
+                    case 3:
+                        $tagCssKlasa.=" crkvaManastir";
+                        break;
+                    case 4:
+                        $tagCssKlasa.=" tvrdjava";
+                        break;
+                    case 5:
+                        $tagCssKlasa.=" areoloskoNalaziste";
+                        break;
+                    case 6:
+                        $tagCssKlasa.=" parkPrirode";
+                        break;
+                }
+            }
+            array_push($tagoviCssKlase, $tagCssKlasa);
+        }
+        
+        $this->prikaz("headerPisac", "objave", ["kontroler" => "Pisac", "objave" => $objave, "autori" => $autori, "tagoviCssKlase" => $tagoviCssKlase]);
+    }
+    
+     /**
+     * Funkcija koja obavlja lodjavljivanje korisnika
+     *
+     * @return void
+     */
+    public function izlogujSe() {
+        $this->session->destroy();
+        return redirect()->to(site_url('Gost'));
     }
 }
--- Implementacija/explore_serbia/app/Views/stranice/headerPisac.php
+++ Implementacija/explore_serbia/app/Views/stranice/headerPisac.php
@@ -1,5 +1,6 @@
 <!-- by Marko Jovanovic 2018/0607 -->
 <!--by Miloš Brković 0599/2019-->
+<!--by Nikola Bjelobaba 0442/2019-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +11,9 @@
         <a class="navbar-brand" href="#">
             <img src="/assets/images/logo/PSI-ES-Logo-Transparent.png" width="30" height="30" alt="">
         </a>
-        <a class="navbar-brand" href="ulogovaniKorisnik.html">Explore Serbia</a>
+        <a class="navbar-brand" href="/">Explore Serbia</a>
+        
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -18,7 +21,25 @@
         
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Filteri
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <span class="dropdown-item">Istorijska ličnost</span>
+                        <span class="dropdown-item">Spomenik</span>
+                        <span class="dropdown-item">Crkva/manastir</span>
+                        <span class="dropdown-item">Tvrdjava</span>
+                        <span class="dropdown-item">Arheološko nalazište</span>
+                        <span class="dropdown-item">Park prirode</span>
+                    </div>
+                </li>
+                <li>
+                    <form method="GET" action="<?php echo site_url("Pisac/pretraga"); ?>" class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" name="pretraga" placeholder="Pretraži" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pretraži</button>
+                    </form>
+                </li>
             </ul>
             
             <form method="get" action="kreiranjeObjave.html">
@@ -28,6 +49,6 @@
             <button  class="btnAdm"><i class="fa fa-user" aria-hidden="true"></i></button>
             </form>
             <form method="get" action="./podesavanje.html"><button class="btnAdm"><i class="fa fa-cogs"></i></button> </form>
-            <a href="index.html" style="margin-left: 10px;">Izloguj se</a>
+            <a href="<?php echo site_url("/Pisac/izlogujSe"); ?>" style="margin-left: 10px;">Izloguj se</a>
         </div>
     </nav>

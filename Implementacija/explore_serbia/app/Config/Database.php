7577526d4a0ab5d163d5f66dbcd06ae26c79ddaf
remaxsrb <remaxsrb@protonmail.com>
2022-05-20 19:37:01 +0200

    fixed a typo in Database.php

--- Implementacija/explore_serbia/app/Config/Database.php
+++ Implementacija/explore_serbia/app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '!',
+        'username' => '',
+        'password' => '',
         'database' => 'exploreserbia',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
